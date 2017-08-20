5062d89ea4022a934c5f88bda3609ce81dcbb46a
algosig <sigdelsanjog@gmail.com>
2017-08-21 00:30:02 +0545

    basic routing to check if express is running

--- app/app.js
+++ app/app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var app = express();
 
 var port = 5000
+
+app.get('/', function(req, res){
+    res.send('ELibrary');
+});
+
+app.get('/books', function(req, res){
+    res.send('Books')
+})
+
 app.listen(5000, function(err){
     console.log('rinning server on port', + port)
 });
